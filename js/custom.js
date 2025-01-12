0706a4a765b31ed862726074fe42903744e6427e
AHMED-BIN-ASIF <hafizahmed2007@gmial.com>
2025-01-12 07:00:56 +0500

    updates

--- js/custom.js
+++ js/custom.js
@@ -68,22 +68,7 @@ $(document).ready(function () {
       },
     ],
   });
-  $("#videoModal").on("shown.bs.modal", function () {
-    successSlick.slick("slickPause");
-    $("#videoPlayer")[0].play();
-  });
 
-  $("#close_btn").click(function () {
-    successSlick.slick("slickPlay");
-    $("#videoPlayer")[0].pause();
-    $("#videoModal").modal("hide");
-  });
-
-  $(".success_box").click(function () {
-    var videoPath = $(this).data("video");
-    $("#videoPlayer").attr("src", videoPath);
-    $("#videoModal").modal("show");
-  });
   // Demo Slider
   $(".demo-slider").slick({
     dots: false,
@@ -105,27 +90,49 @@ $(document).ready(function () {
       },
     ],
   });
-  // Success Slick Slider
-  $(".success_slick").slick({
-    dots: false,
-    infinite: true,
-    draggable: true,
-    speed: 800,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    pauseOnHover: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    prevArrow: '<div class="slick-prev custom_slick_prev_success"></div>',
-    nextArrow: '<div class="slick-next custom_slick_next_success"></div>',
-    responsive: [
-      { breakpoint: 768, settings: { arrows: false, slidesToShow: 2 } },
-      {
-        breakpoint: 480,
-        settings: { arrows: false, slidesToShow: 1, dots: true },
-      },
-    ],
-  });
+ // Success Slick Slider
+$(".success_slick").slick({
+  dots: false,
+  infinite: true,
+  draggable: true,
+  speed: 800,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  pauseOnHover: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  prevArrow: '<div class="slick-prev custom_slick_prev_success"></div>',
+  nextArrow: '<div class="slick-next custom_slick_next_success"></div>',
+  responsive: [
+    { breakpoint: 768, settings: { arrows: false, slidesToShow: 2 } },
+    {
+      breakpoint: 480,
+      settings: { arrows: false, slidesToShow: 1, dots: true },
+    },
+  ],
+});
+
+// Pause the slick slider and play video when modal is shown
+$("#videoModal").on("shown.bs.modal", function () {
+  $(".success_slick").slick("slickPause"); // Pause slider
+  $("#videoPlayer")[0].play(); // Play the video
+});
+
+// Resume the slick slider and pause video when closing modal
+$("#close_btn").click(function () {
+  $(".success_slick").slick("slickPlay"); // Resume slider
+  $("#videoPlayer")[0].pause(); // Pause the video
+  $("#videoPlayer")[0].currentTime = 0; // Reset video to start
+  $("#videoModal").modal("hide"); // Hide the modal
+});
+
+// Dynamically load the video source and show modal
+$(".success_box").click(function () {
+  var videoPath = $(this).data("video");
+  $("#videoPlayer").attr("src", videoPath); // Set video source
+  $("#videoModal").modal("show"); // Show modal
+});
+
   // Demo Iframe Handling
   $(".demo-slider img").on("click", function () {
     const clickedDataId = $(this).data("id");
