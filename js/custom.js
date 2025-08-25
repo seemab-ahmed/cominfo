a53cd3a5544a38314f9263c38eee7b9c9046d076
Seemab Ahmed <seemab40615@gmail.com>
2025-08-25 12:20:41 +0500

    ComInfo

--- js/custom.js
+++ js/custom.js
@@ -23,13 +23,12 @@ $(document).ready(function () {
   $(".testimonials-slider").slick({
     dots: true,
     infinite: true,
-    speed: 1000,
+    speed: 800,
     autoplay: true,
-    autoplaySpeed: 2500,
+    autoplaySpeed: 3000,
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 4000,
     arrows: false,
     responsive: [
       { breakpoint: 1024, settings: { slidesToShow: 2 } },
